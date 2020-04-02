dd0974db2d537759e9726e39ff703f8101dba546
Kudzai Chasinda <kudzai.chasinda@gmail.com>
2020-04-02 02:07:49 +0200

    added game over fragment

--- app/src/main/java/com/quarantine/thirtyseconds/ui/gameover/GameOverFragment.kt
+++ app/src/main/java/com/quarantine/thirtyseconds/ui/gameover/GameOverFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.quarantine.thirtyseconds.databinding.FragmentGameOverBinding
 
 class GameOverFragment : Fragment() {
 
@@ -13,6 +14,10 @@ class GameOverFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        val binding = FragmentGameOverBinding.inflate(inflater, container, false)
+        binding.apply {
+
+        }
+        return binding.root
     }
 }
