85db210b056264c0f2c970ad14158e11ef31b1bc
Kudzai Chasinda <kudzai.chasinda@gmail.com>
2020-04-09 15:43:16 +0200

    added preference utils helper class

---
+++ app/src/main/java/com/quarantine/thirtyseconds/utils/PreferencesUtils.kt
@@ -0,0 +1,27 @@
+package com.quarantine.thirtyseconds.utils
+
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
+import androidx.core.content.edit
+
+class PreferencesUtils(context: Context) {
+
+    companion object {
+        const val SHARED_PREFERENCES_NAME = "thirty_seconds_prefs"
+        const val KEY_IS_FIRST_TIME = "is_first_time"
+    }
+
+    private val sharedPreferences: SharedPreferences =
+        context.getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE)
+
+    fun isUserFirstTime(): Boolean {
+        return sharedPreferences.getBoolean(KEY_IS_FIRST_TIME, false)
+    }
+
+    fun makeUserFirstTime() {
+        sharedPreferences.edit(commit = true) {
+            putBoolean(KEY_IS_FIRST_TIME, true)
+        }
+    }
+}
